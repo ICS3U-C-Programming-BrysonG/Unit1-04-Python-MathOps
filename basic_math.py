ea9b9caa19dae1a53a24dcc8741db2c5f2fca54a
Bryson Grant <Bryson.grant@stu.ocsb.ca>
2026-09-16 13:25:39 +0000

    Added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Created By: Bryson Grant
+# Date: 09 16, 2026
+# This program does basic math.
+
+
+def main():
+    print("7-3={}".format(7 - 3))
+    print("3+5={}".format(3 + 5))
+    print("8*6={}".format(8 * 6))
+    print("4/2={}".format(4 / 2))
+    print("2+5*3={}".format(2 + 5 * 3))
+    print("2^3={}".format(2 ^ 3))  # bitwise operator
+    print("2**3={}".format(2**3))  # exponent
+    print("16**0.5={}".format(16**0.5))  # square root
+
+
+if __name__ == "__main__":
+    main()
